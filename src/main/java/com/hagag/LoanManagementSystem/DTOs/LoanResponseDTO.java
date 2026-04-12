fd2476b466edaeb89db04c3c99dc651e1295f1b1
yousef hagag <hagagyousef927@gmail.com>
2026-04-12 23:24:58 +0200

    added static buildResponse(); to reduce code

--- src/main/java/com/hagag/LoanManagementSystem/DTOs/LoanResponseDTO.java
+++ src/main/java/com/hagag/LoanManagementSystem/DTOs/LoanResponseDTO.java
@@ -1,7 +1,10 @@
 package com.hagag.LoanManagementSystem.DTOs;
 
+import com.hagag.LoanManagementSystem.entities.Loan;
 import com.hagag.LoanManagementSystem.entities.Status;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class LoanResponseDTO {
@@ -10,6 +13,16 @@ public class LoanResponseDTO {
     private double amount;
     private int durationMonths;
     private Status status;
-    private String createdAt;
+    private LocalDateTime createdAt;
+
+    public static LoanResponseDTO buildResponseFromLoan(Loan loan) {
+        LoanResponseDTO response = new LoanResponseDTO();
+        response.setId(loan.getId());
+        response.setAmount(loan.getAmount());
+        response.setDurationMonths(loan.getDurationMonths());
+        response.setStatus(loan.getStatus());
+        response.setCreatedAt(loan.getCreatedAt());
+        return response;
+    }
 
 }
