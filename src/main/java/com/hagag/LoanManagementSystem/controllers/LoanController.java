cf0a4e9f7310321d8c43e5d5b567256505c1f1c1
yousef hagag <hagagyousef927@gmail.com>
2026-04-12 23:28:03 +0200

    added apply for loan api

--- src/main/java/com/hagag/LoanManagementSystem/controllers/LoanController.java
+++ src/main/java/com/hagag/LoanManagementSystem/controllers/LoanController.java
@@ -1,21 +1,33 @@
 package com.hagag.LoanManagementSystem.controllers;
 
+import com.hagag.LoanManagementSystem.DTOs.LoanRequestDTO;
 import com.hagag.LoanManagementSystem.DTOs.LoanResponseDTO;
+import com.hagag.LoanManagementSystem.services.LoanServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoanController {
+
+    @Autowired
+    LoanServices loanServices;
 
     @GetMapping("/")
     public String home() {
         return "redirect:/swagger-ui/index.html";
     }
 
-//    @PostMapping("/apply")
-//    public ResponseEntity<LoanResponseDTO>
+    @PostMapping("/apply")
+    public ResponseEntity<LoanResponseDTO> applyLoan(@RequestBody LoanRequestDTO loanRequestDTO) {
+
+        return loanServices.applyLoan(loanRequestDTO);
+
+    }
 
 
 }
