e8e49348f0b2be2067d48859555ebef0d878d942
yousef hagag <hagagyousef927@gmail.com>
2026-04-12 23:23:13 +0200

    added apply(); for loan logic

--- src/main/java/com/hagag/LoanManagementSystem/services/LoanServices.java
+++ src/main/java/com/hagag/LoanManagementSystem/services/LoanServices.java
@@ -1,9 +1,48 @@
 package com.hagag.LoanManagementSystem.services;
 
+import com.hagag.LoanManagementSystem.DTOs.LoanRequestDTO;
+import com.hagag.LoanManagementSystem.DTOs.LoanResponseDTO;
+import com.hagag.LoanManagementSystem.daos.LoanRepository;
+import com.hagag.LoanManagementSystem.daos.UserRepository;
+import com.hagag.LoanManagementSystem.entities.Loan;
+import com.hagag.LoanManagementSystem.entities.Status;
+import com.hagag.LoanManagementSystem.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class LoanServices {
 
+    @Autowired
+    UserRepository userRepository;
 
+    @Autowired
+    LoanRepository loanRepository;
+
+
+    public ResponseEntity<LoanResponseDTO> applyLoan(LoanRequestDTO loanRequestDTO) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String email = auth.getName();
+
+        User user = userRepository.findByEmail(email);
+
+        Loan loan = new Loan();
+        loan.setAmount(loanRequestDTO.getAmount());
+        loan.setDurationMonths(loanRequestDTO.getDurationMonths());
+        loan.setStatus(Status.PENDING);
+        loan.setCreatedAt(LocalDateTime.now());
+        loan.setUser(user);
+
+        loanRepository.save(loan);
+
+        LoanResponseDTO responseDTO = LoanResponseDTO.buildResponseFromLoan(loan);
+
+        return new ResponseEntity<>(responseDTO , HttpStatus.CREATED);
+    }
 }
