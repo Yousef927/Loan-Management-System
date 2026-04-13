15eea28558b9046cb4b904de8b2abdd3b50931de
yousef hagag <hagagyousef927@gmail.com>
2026-04-13 18:31:41 +0200

    added swagger and register user API

---
+++ src/main/java/com/hagag/LoanManagementSystem/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.hagag.LoanManagementSystem.controllers;
+
+
+import com.hagag.LoanManagementSystem.DTOs.UserRequestDTO;
+import com.hagag.LoanManagementSystem.DTOs.UserResponseDTO;
+import com.hagag.LoanManagementSystem.entities.User;
+import com.hagag.LoanManagementSystem.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserServices userServices;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponseDTO> registerUser(@RequestBody UserRequestDTO user) {
+        return userServices.registerUser(user);
+
+    }
+}
