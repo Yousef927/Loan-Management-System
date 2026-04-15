06165ca04cad21ac16a6e8d08caa399a4501f05b
yousef hagag <hagagyousef927@gmail.com>
2026-04-15 23:22:49 +0200

    added find loans for current user added in ORM

--- src/main/java/com/hagag/LoanManagementSystem/daos/LoanRepository.java
+++ src/main/java/com/hagag/LoanManagementSystem/daos/LoanRepository.java
@@ -1,7 +1,14 @@
 package com.hagag.LoanManagementSystem.daos;
 
 import com.hagag.LoanManagementSystem.entities.Loan;
+import com.hagag.LoanManagementSystem.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LoanRepository extends JpaRepository<Loan, Integer> {
+
+
+    List<Loan> findByUser(User user);
 }
