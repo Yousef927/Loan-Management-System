d6d935ccba6b35ee0f60e054ccd15354c1fc5a44
yousef hagag <hagagyousef927@gmail.com>
2026-04-22 23:45:36 +0200

    added validation and business logic validation to fields

--- src/main/java/com/hagag/LoanManagementSystem/DTOs/LoanRequestDTO.java
+++ src/main/java/com/hagag/LoanManagementSystem/DTOs/LoanRequestDTO.java
@@ -1,11 +1,17 @@
 package com.hagag.LoanManagementSystem.DTOs;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class LoanRequestDTO {
 
+    @NotNull
+    @Min(value = 800000 , message = "Minimum loan amount is 800,000")
     private double amount;
+
+    @Min(value = 12 , message = "Minimum loan duration is 12 months")
     private int durationMonths;
 
 }
--- src/main/java/com/hagag/LoanManagementSystem/DTOs/UserRequestDTO.java
+++ src/main/java/com/hagag/LoanManagementSystem/DTOs/UserRequestDTO.java
@@ -1,13 +1,17 @@
 package com.hagag.LoanManagementSystem.DTOs;
 
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class UserRequestDTO {
 
+    @NotNull
     private String name;
+    @NotNull
     private String email;
+    @NotNull
     private String password;
 
 }
