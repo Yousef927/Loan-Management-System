e719fbac67703b5c575340a2899f20ed30eeabbb
yousef hagag <hagagyousef927@gmail.com>
2026-04-26 23:55:01 +0300

    ORM method added

--- src/main/java/com/hagag/LoanManagementSystem/daos/LoanHistoryRepository.java
+++ src/main/java/com/hagag/LoanManagementSystem/daos/LoanHistoryRepository.java
@@ -3,5 +3,8 @@ package com.hagag.LoanManagementSystem.daos;
 import com.hagag.LoanManagementSystem.entities.LoanHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface LoanHistoryRepository extends JpaRepository<LoanHistory , Integer> {
+    List<LoanHistory> findByLoanId(Integer loanId);
 }
