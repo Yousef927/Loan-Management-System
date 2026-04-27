361eeeb30c4081cb2c2f07f8b95d090a634a7153
yousef hagag <hagagyousef927@gmail.com>
2026-04-27 23:33:24 +0300

    build loan history response method added

--- src/main/java/com/hagag/LoanManagementSystem/DTOs/LoanHistoryResponseDTO.java
+++ src/main/java/com/hagag/LoanManagementSystem/DTOs/LoanHistoryResponseDTO.java
@@ -1,12 +1,23 @@
 package com.hagag.LoanManagementSystem.DTOs;
 
 import com.hagag.LoanManagementSystem.entities.Action;
+import com.hagag.LoanManagementSystem.entities.LoanHistory;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class LoanHistoryResponseDTO {
 
     Action action;
     String actedBy;
-    String actedAt;
+    LocalDateTime actedAt;
+
+    public static LoanHistoryResponseDTO buildResponseFromHistory(LoanHistory history) {
+        LoanHistoryResponseDTO response = new LoanHistoryResponseDTO();
+        response.setAction(history.getAction());
+        response.setActedBy(history.getActedBy());
+        response.setActedAt(history.getActedAt());
+        return response;
+    }
 }
