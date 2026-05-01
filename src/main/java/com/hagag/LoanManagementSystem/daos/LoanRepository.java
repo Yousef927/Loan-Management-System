628938d14ce247337083705934e945d02e721f22
yousef hagag <hagagyousef927@gmail.com>
2026-05-01 22:50:24 +0300

    just formatted the code , changed ORM method return type as well

--- src/main/java/com/hagag/LoanManagementSystem/daos/LoanRepository.java
+++ src/main/java/com/hagag/LoanManagementSystem/daos/LoanRepository.java
@@ -3,6 +3,8 @@ package com.hagag.LoanManagementSystem.daos;
 import com.hagag.LoanManagementSystem.entities.Loan;
 import com.hagag.LoanManagementSystem.entities.Status;
 import com.hagag.LoanManagementSystem.entities.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -11,8 +13,8 @@ import java.util.List;
 public interface LoanRepository extends JpaRepository<Loan, Integer> {
 
 
-    List<Loan> findByUser(User user);
+    Page<Loan> findByUser(User user , Pageable pageable);
 
 
-    List<Loan> findByUserAndStatus(User user , Status status);
+    Page<Loan> findByUserAndStatus(User user , Status status , Pageable pageable);
 }
