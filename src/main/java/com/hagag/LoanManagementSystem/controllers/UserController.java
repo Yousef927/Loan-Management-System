f8fbd849172b44ec27e5a899ff271446727d34c2
yousef hagag <hagagyousef927@gmail.com>
2026-05-04 03:17:06 +0300

    added register as an officer API

--- src/main/java/com/hagag/LoanManagementSystem/controllers/UserController.java
+++ src/main/java/com/hagag/LoanManagementSystem/controllers/UserController.java
@@ -9,9 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
 
     @Autowired
@@ -20,6 +22,10 @@ public class UserController {
     @PostMapping("/register")
     public ResponseEntity<UserResponseDTO> registerUser(@RequestBody UserRequestDTO user) {
         return userServices.registerUser(user);
+    }
 
+    @PostMapping("/register-officer")
+    public ResponseEntity<UserResponseDTO> registerOfficer(@RequestBody UserRequestDTO user) {
+        return userServices.registerOfficer(user);
     }
 }
