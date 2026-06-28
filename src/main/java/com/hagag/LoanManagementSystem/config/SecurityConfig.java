75b779b872cc95412c4df92f8788601c538f954f
yousef hagag <hagagyousef927@gmail.com>
2026-06-29 01:07:05 +0300

    put the Jwt filter in order to override unampassauthfilter + added authmanager bean

--- src/main/java/com/hagag/LoanManagementSystem/config/SecurityConfig.java
+++ src/main/java/com/hagag/LoanManagementSystem/config/SecurityConfig.java
@@ -3,15 +3,17 @@ package com.hagag.LoanManagementSystem.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -20,15 +22,19 @@ public class SecurityConfig {
     @Autowired
     UserDetailsService userDetailsService;
 
+    @Autowired
+    JwtFilter jwtFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/users/register" , "/api/users/register-officer" ).permitAll()
+                        .requestMatchers("/api/users/register" , "/api/users/register-officer" , "/api/users/login" ).permitAll()
                         .anyRequest()
                         .authenticated())
                 .httpBasic(Customizer.withDefaults())
+                .addFilterBefore( jwtFilter , UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
@@ -47,6 +53,11 @@ public class SecurityConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
 
         return authProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configurer) {
+        return configurer.getAuthenticationManager();
     }
 
 }
