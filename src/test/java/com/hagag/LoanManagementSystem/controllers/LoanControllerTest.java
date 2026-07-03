63f6261b2883a85dc7e8447b21094605a427b834
yousef hagag <hagagyousef927@gmail.com>
2026-07-03 03:32:47 +0300

    added tests for all loan 6 APIs

--- src/test/java/com/hagag/LoanManagementSystem/controllers/LoanControllerTest.java
+++ src/test/java/com/hagag/LoanManagementSystem/controllers/LoanControllerTest.java
@@ -7,8 +7,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 
 
 
@@ -22,6 +25,8 @@ public class LoanControllerTest {
     @Autowired
     JwtService jwtService;
 
+    Integer loanId = 7;
+
 
     String jsonRequest = """
             {
@@ -34,7 +39,7 @@ public class LoanControllerTest {
     @Test
     void applyForLoanTest() throws Exception {
 
-        String token = jwtService.generateToken("anasahmed927@gmail.com");
+        String token = getTokenForUser();
 
         mockMvc.perform(
                 post("/api/loans/apply")
@@ -43,5 +48,85 @@ public class LoanControllerTest {
                         .content(jsonRequest)
         )
                 .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void approveLoanTest() throws Exception {
+
+        String token = getTokenForOfficer();
+
+        mockMvc.perform(
+                post("/api/loans/approve/" + loanId)
+                        .header("Authorization", "Bearer "+ token)
+
+        )
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+
+    }
+
+    @Test
+    void rejectLoanTest() throws  Exception {
+
+        String token = getTokenForOfficer();
+
+        mockMvc.perform(
+                post("/api/loans/reject/" + loanId)
+                        .header("Authorization", "Bearer "+ token)
+        )
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    void getMyLoansTest() throws Exception {
+
+        String token = getTokenForUser();
+
+        mockMvc.perform(
+                get("/api/loans/myloans")
+                        .header("Authorization", "Bearer "+ token)
+                        .param("status" , "APPROVED")
+                        .param("page" , "0")
+                        .param("size" , "5")
+                        .param("sortBy" , "createdAt")
+        )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getLoanTest() throws Exception {
+
+        String token = getTokenForUser();
+
+        mockMvc.perform(
+                get("/api/loans/loan/" + loanId)
+                        .header("Authorization", "Bearer " + token)
+        )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getLoanHistoryTest() throws Exception {
+
+        String token = getTokenForUser();
+
+        mockMvc.perform(
+                get("/api/loans/loan/" + loanId + "/history")
+                        .header("Authorization" , "Bearer " + token)
+        )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    String getTokenForUser() {
+        String token = jwtService.generateToken("anasahmed927@gmail.com");
+        return token;
+    }
+    String getTokenForOfficer() {
+        String token = jwtService.generateToken("officer927@gmail.com");
+        return token;
     }
 }
