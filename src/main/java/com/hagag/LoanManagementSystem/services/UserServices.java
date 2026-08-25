1b8d90c7ceadfeab76e0f785239404962c9d7d6c
yousef hagag <joehyde927@gmail.com>
2026-08-25 23:41:03 +0300

    removed spaces

--- src/main/java/com/hagag/LoanManagementSystem/services/UserServices.java
+++ src/main/java/com/hagag/LoanManagementSystem/services/UserServices.java
@@ -61,8 +61,7 @@ public class UserServices {
 
         return new ResponseEntity<>(responseDTO , HttpStatus.CREATED);
 
-
-
+        
     }
 
     public String verifyLogin(LoginRequestDTO loginRequestDTO) {
