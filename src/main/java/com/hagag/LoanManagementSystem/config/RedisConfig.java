a4eb3c33e4ab9da358d7343bb99b74e5050a097a
Yousef hagag <hagagyousef927@gmail.com>
2026-09-17 02:04:32 +0300

    removed useless packages from REDIS config

--- src/main/java/com/hagag/LoanManagementSystem/config/RedisConfig.java
+++ src/main/java/com/hagag/LoanManagementSystem/config/RedisConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import tools.jackson.databind.ObjectMapper;
+
 
 import java.time.Duration;
 
