f3dc30e5e69fa000054f43492e2aa0ccd70b50b2
Yousef hagag <hagagyousef927@gmail.com>
2026-09-17 02:10:52 +0300

    removed useless packages from loan DAO/repo

--- src/main/java/com/hagag/LoanManagementSystem/daos/LoanRepository.java
+++ src/main/java/com/hagag/LoanManagementSystem/daos/LoanRepository.java
@@ -6,7 +6,7 @@ import com.hagag.LoanManagementSystem.entities.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+
 
 import java.util.List;
 
